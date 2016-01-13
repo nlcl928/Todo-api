735cbd158f610f93c050f5668c240fce7208dfba
shufanwang <wsf07nick@gmail.com>
2016-01-12 22:40:06 -0600

    add put

--- server.js
+++ server.js
@@ -72,6 +72,35 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'complete'); 
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('complete') && _.isBoolean(body.complete)) {
+		validAttributes.complete = body.complete;
+	} else if (body.hasOwnProperty('complete')) {
+		// Bad
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+	
+});
+
 app.listen(PORT, function() {
 	console.log('Express listening on PORT ' + PORT + '!');
 });
