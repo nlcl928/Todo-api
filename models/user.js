d13acffad9a6ccb38eb70a365114872f91af9175
shufanwang <wsf07nick@gmail.com>
2016-01-16 14:51:56 -0600

    Convert email to lower case

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function (sequelize, DataType) {
 				len: [7, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options) {
+				// user.email to lowercase only if it is a string
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 }
