b1016123f9e0829180938fbe19aa4387a385ae45
shufanwang <wsf07nick@gmail.com>
2016-01-17 18:52:44 -0600

    Added todo user association

--- server.js
+++ server.js
@@ -85,7 +85,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'complete'); //use _.pick to only pick description and complete
 
 	db.todo.create(body).then(function (todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then (function () {
+			return todo.reload();
+		}).then(function (todo) {
+			res.json(todo.toJSON());
+		});
 	}, function (e) {
 		res.status(400).json(e);
 	});
